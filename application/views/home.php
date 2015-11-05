8f4724c3d0ad4fb859c2700df5909ba10bf3db4c
Matheusqualquercoisa <matheus@getstudio.net>
2015-11-05 00:59:25 -0200

    Teste de commit
    Eu mechi na home ,.. foda se

--- application/views/home.php
+++ application/views/home.php
@@ -31,7 +31,7 @@
     text-align: -webkit-center;
     text-align:center;
         font-size: 30px;">
-                <b>Mais que uma cadeira
+                <b>Caguei se meu pog de css ta ai no meio, se quiser arruma ,.. sei que ta falando disso
                 <br>
                 <span style="
 
