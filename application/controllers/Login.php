a2fd9be25eaa6570000635eee5d0bb839bc71cd4
Matheusqualquercoisa <matheus@getstudio.net>
2015-12-03 22:44:50 -0200

    Revert "tela pedidos"
    This reverts commit 394d49a7420e350ba564c8b1d658b02591e0bac9.
    
    Conflicts:
    	application/controllers/Produtos.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,20 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-   /**
-    * Método principal do mini-crud
-    * @param nenhum
-    * @return view
-    */
-
+    public function __construct() {
+            parent::__construct();
+            $this->load->helper('url');
+            $this->load->helper('form');
+            $this->load->helper('array');
+            $this->load->library('form_validation');
+            $this->load->library('table');
+            $this->load->model('categoria_model', 'crud');
+   }
+   
+   
    public function index()
    {
-        $this->form_validation->set_rules('login', 'LOGIN', 'trim|required|min_length[4]|strtolower');
-        $this->form_validation->set_rules('senha', 'SENHA', 'trim|required|min_length[4]|strtolower');
-            if($this->form_validation->run()== TRUE):
-                echo 'Formulario vazio';
-            endif;
-        $variaveis['cadastros'] ="";
         $this->load->view('includes/headLogin');
         $this->load->view('login/login', $variaveis);
    }
--- application/helpers/funcoes_helper.php
+++ application/helpers/funcoes_helper.php
@@ -1,6 +1,6 @@
 <?php
-    function ErrosValidacao() {
-        
+    function ErrosValidacao(){
+        if(validation_errors()) echo '<div class="alert alert-danger" role="alert">'. validation_errors('<p>', '</p>'. '</div>');
 }
 ?>
 
